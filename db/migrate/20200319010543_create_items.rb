5d0cfa1de092817cc7700d6024277c426295c980
izumichika <chikara0707@outlook.jp>
2020-03-19 10:38:52 +0900

    add migrate file
    アイテムのマイグレーション ファイルを追記

--- db/migrate/20200319010543_create_items.rb
+++ db/migrate/20200319010543_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.
+      t.references :user, foreign_key: true
+      t.references :brand, foreign_key: true
+      t.references :category, null: false
+      t.string :name, null: false
+      t.string :description, null: false
+      t.string :status, null: false
+      t.string :price, null: false
+      t.string :shipping_charges, null: false
+      t.string :days_to_ship, null: false
       t.timestamps
     end
   end
