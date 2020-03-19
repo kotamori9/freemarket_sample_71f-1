09c93ccd8e4e15e94c89b93f999c75f732682275
izumichika <chikara0707@outlook.jp>
2020-03-19 14:27:20 +0900

    add db column and type
    userモデルのデータ型と制約を記載

--- db/migrate/20200319052337_devise_create_users.rb
+++ db/migrate/20200319052337_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :email,              null: false,unique: true, default: ""
+      t.string :encrypted_password, null: false,length: { minimum: 7 }, default: ""
+      t.string :birthday,           null: false
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_furigana, null: false
+      t.string :first_name_furigana,null: false
+      t.string :credit_card,        null: false
+      t.string :tell
 
       ## Recoverable
       t.string   :reset_password_token
