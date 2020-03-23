687434daa3eaac20825a1c1341bc9455b1d70b04
izumichika <chikara0707@outlook.jp>
2020-03-23 19:26:55 +0900

    全角バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,12 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 7 }
   validates :birthday, presence: true
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
+  validates :last_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
+  validates :first_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
   validates :credit_card, presence: true
+
   # has_many :items
   # has_many :comments
   has_one :address
