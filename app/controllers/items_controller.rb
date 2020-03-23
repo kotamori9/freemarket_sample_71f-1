93321b773eac056ad81d7811e0affdb3014382fd
kotamori9 <kotamori27@gmail.com>
2020-03-23 20:18:19 +0900

    categoryモデル

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       binding.pry
       redirect_to root_path(@item), flash[:notice] ='商品が出品されました'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_013731) do
+ActiveRecord::Schema.define(version: 2020_03_23_111456) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2020_03_23_013731) do
     t.string "city", null: false
     t.string "address_number", null: false
     t.string "any_address_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
