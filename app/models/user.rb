217346e62177337eb3af14526f54751f6c6c0400
izumichika <chikara0707@outlook.jp>
2020-03-23 19:42:34 +0900

    リファクタリング
    バリデーションをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 7 }
   validates :birthday, presence: true
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
-  validates :last_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
-  validates :first_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
+  validates :last_name,:first_name,:last_name_furigana,:first_name_furigana, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください'}
   validates :credit_card, presence: true
 
   # has_many :items
