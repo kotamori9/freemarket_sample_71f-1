979e80064728dd0ba36aa19445016e76465a1b01
izumichika <chikara0707@outlook.jp>
2020-03-23 18:02:22 +0900

    住所登録の実装

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,9 +5,16 @@ class AddressesController < ApplicationController
   end
 
   def create
+    @address = Address.new(adress_params)
+    if @address.save
+      redirect_to root_path, notice: '住所を登録しました'
+    else
+      render :new
+    end
   end
 
-  def zipedit
-    params.require(:address).permit(:postal_code, :prefecture, :city, :address_number, :any_address_info)
+  private
+  def adress_params
+    params.require(:address).permit(:destination_name, :destination_name_furigana, :postal_code, :prefecture, :city, :address_number, :any_address_info).merge(user_id: current_user.id)
   end
 end
