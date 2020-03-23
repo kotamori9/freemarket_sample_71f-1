dd8c45105fe3da80649937092836f12872830e2f
izumichika <chikara0707@outlook.jp>
2020-03-23 12:50:01 +0900

    住所・マイページをルーティング
    住所コントローラにnewアクションを設定
    マイページから住所登録をするようにルーティング
    ログイン後マイページに偏移できるようにルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resource :items, only: [:index,:new]
+  resource :users, only: [:show] do
+    resource :addresses, only: [:new, :create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
