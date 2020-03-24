76d2e24a68ee458994d6d290891857133ec71fd4
makkei0526 <makkei1710@gmail.com>
2020-03-24 11:33:43 +0900

    スキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_114613) do
+ActiveRecord::Schema.define(version: 2020_03_24_013801) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -33,18 +33,26 @@ ActiveRecord::Schema.define(version: 2020_03_23_114613) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
+  create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "user"
-    t.string "brand"
-    t.string "category", null: false
+    t.integer "user"
+    t.integer "brand"
+    t.integer "category", null: false
     t.string "name", null: false
     t.string "description", null: false
     t.string "status", null: false
-    t.integer "price", null: false
+    t.string "price", null: false
     t.string "shipping_charges", null: false
     t.string "days_to_ship", null: false
-    t.integer "buyer_id"
-    t.integer "saler_id"
+    t.string "buyer_id"
+    t.string "saler_id"
     t.string "area", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
