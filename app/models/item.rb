f47b458262bdb17f5f047134c9852e68955b6b3f
makkei0526 <makkei1710@gmail.com>
2020-03-26 11:25:48 +0900

    本番環境でエラーが出るのでコメントアウトを外した

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :statushash
   belongs_to_active_hash :postagehash
   belongs_to_active_hash :shippinghash
-  # belongs_to_active_hash :Aprefecture
+  belongs_to_active_hash :Aprefecture
 end
 
 
