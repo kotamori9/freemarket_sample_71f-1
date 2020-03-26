4c09ed30ffa452dfe80d681adf1851f704228ad8
izumichika <chikara0707@outlook.jp>
2020-03-26 15:47:44 +0900

    モデルを適用

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -15,7 +15,7 @@ class PurchaseController < ApplicationController
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
+    card = Creditcard.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
