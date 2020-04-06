1d6012a635e5be886f86c89d968c4bf48c4e95e0
izumichika <chikara0707@outlook.jp>
2020-04-06 20:29:50 +0900

    add model

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,14 @@ class Address < ApplicationRecord
   include JpPrefecture
   jp_prefecture :prefecture,method_name: :pref
 
+  validates :user_id,                   null: false
+  validates :destination_name,           null: false
+  validates :destination_name_furigana,  null: false
+  validates :postal_code,                null: false
+  validates :prefecture,                 null: false
+  validates :city,                       null: false
+  validates :address_number,             null: false
+
   def prefecture_name
     JpPrefecture::Prefecture.find(code: prefecture).try(:name)
   end
